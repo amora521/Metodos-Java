f04a11093fcb4fd74c2f9382be044d395555c77c
Areum <paloma.mmendonca@hotmail.com>
2022-07-24 16:39:19 -0300

    Atualização do exercício 2

--- src/one/dio/bootcamp/exercicio2/Main.java
+++ src/one/dio/bootcamp/exercicio2/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main2(String[] args){
+    public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
         int valor1, valor2, valor3;
@@ -13,13 +13,19 @@ public class Main {
 //        Quadrado
         System.out.println("Informe o um valor: ");
         valor1 = scan.nextInt();
-        Quadrilatero.area(valor1);
+//        Quadrilatero.area(valor1); -- tipo de resolução com método void
+        double areaQuadrado = Quadrilatero.area(valor1);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
 //        Retangulo
         System.out.println("Informe o um valor: ");
         valor1 = scan.nextInt();
         System.out.println("Informe o outro valor: ");
         valor2 = scan.nextInt();
-        Quadrilatero.area(valor1,valor2);
+//        Quadrilatero.area(valor1,valor2); -- tipo de resolução com método void
+        double areaRetangulo = Quadrilatero.area(valor1, valor2);
+        System.out.println("Área do retângulo: " + areaRetangulo);
+
 //        Trapezio
         System.out.println("Informe o um valor: ");
         valor1 = scan.nextInt();
@@ -27,6 +33,9 @@ public class Main {
         valor2 = scan.nextInt();
         System.out.println("Informe o último valor: ");
         valor3 = scan.nextInt();
-        Quadrilatero.area(valor1,valor2,valor3);
+//        Quadrilatero.area(valor1,valor2,valor3); -- tipo de resolução com método void
+        double areaTrapezio = Quadrilatero.area(valor1, valor2, valor3);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+
     }
 }
